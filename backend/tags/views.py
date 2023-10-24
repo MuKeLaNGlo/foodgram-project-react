3d903b5057fdf91bc6547a7eafb4c6ab7e19b9d9
MuKeLaNGlo <steammike23@gmail.com>
2023-10-24 19:28:18 +0500

    fix after review

--- backend/tags/views.py
+++ backend/tags/views.py
@@ -1,5 +1,4 @@
-from rest_framework import status, viewsets
-from rest_framework.response import Response
+from rest_framework import viewsets
 
 from tags.models import Tag
 from tags.serializers import TagSerializer
